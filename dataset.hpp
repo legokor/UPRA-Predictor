aafdfff427437c3a0cc3e1f899ea4490d7674ad5
Dániel Móna <mona.daniel97@gmail.com>
2018-05-24 12:44:44 +0200

    Dataset improvements

--- dataset.hpp
+++ dataset.hpp
@@ -1,4 +1,5 @@
 #pragma once
+#include <algorithm>
 #include <map>
 #include <vector>
 #include "vecs.h"
@@ -19,9 +20,13 @@ public:
     }
 
     void addDataPoint(K k, V v) {
-        data.push_back(std::make_pair(k, v));
+        auto dpoint = std::make_pair(k,v);
+        data.insert(
+            std::upper_bound(data.begin(), data.end(), dpoint,
+            [] (auto a, auto b) { return a.first < b.first; }),
+            dpoint
+        );
     }
-
 
     K getLastKey() {
         return data.back().first;
@@ -33,12 +38,15 @@ public:
 
     // TODO: Check if getting data from outside dataset bounds returns nonsense or not
     V getAt(K k) {
+        //Key smaller than smallest in dataset -> extrapolate
         if (k < data.front().first)
             return interpolate(data[0], data[1], k);
-        for (size_t i = 0; i < data.size()-1; i++) {
-            if (data[i].first < k && k < data[i+1].first)
-                return interpolate(data[i], data[i+1], k);
-        }
+
+        auto pos = std::upper_bound(data.begin(), data.end(), k, [] (K a, auto b) { return a < b.first; });
+        if (pos < data.end())
+            return interpolate(*(pos-1), *pos, k);
+
+        //Key larger than largest in dataset -> extrapolate
         return interpolate(data[data.size()-2], data[data.size()-1], k);
     }
 };
