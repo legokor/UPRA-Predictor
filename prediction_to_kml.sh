c170897f2187c4a51efba9d2e3794835f68c57e9
Dániel Móna <mona.daniel97@gmail.com>
2018-06-08 21:25:59 +0200

    Add shell script for converting CSV fmt predictions to KML

---
+++ prediction_to_kml.sh
@@ -0,0 +1,27 @@
+echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>
+<kml xmlns=\"http://www.opengis.net/kml/2.2\">
+<Document>
+<name>Predicted Flight Path</name>
+<description><![CDATA[Flight data for flight ]]></description>
+<Style id=\"bluePoly\">
+<LineStyle>
+<color>0f00ffff</color>
+<width>4</width>
+</LineStyle>
+<PolyStyle>
+<color>7f00ff00</color>
+</PolyStyle>
+</Style>
+<Placemark>
+<name>Predicted Flight Path</name>
+<description>Flight Data</description>
+<styleUrl>#bluePoly</styleUrl>
+<LineString>
+<extrude>1</extrude>
+<tesselate>1</tesselate>
+<altitudeMode>absolute</altitudeMode>
+<coordinates>
+`cat $1 | cut --delimiter=',' --complement --fields=1`
+</coordinates>
+</LineString></Placemark>
+</Document></kml>"
